85e554ed7a7e7e456bfd54e8d01cb8ff066c9e6a
ryooit <ryooit70@gmail.com>
2021-11-10 18:22:42 +0900

    Edit init.sh

--- init.sh
+++ init.sh
@@ -2,14 +2,19 @@
 sudo apt-get update
 sudo apt-get install -y sox bc
 
+cd espnet
 pip install --upgrade pip
 pip install -q -e .
 
 # download pre-compiled warp-ctc and kaldi tools
 pip install gdown
-espnet/utils/download_from_google_drive.sh \
+utils/download_from_google_drive.sh \
     "https://drive.google.com/open?id=13Y4tSygc8WtqzvAVGK_vRV9GlV7TRC0w" espnet/tools tar.gz > /dev/null
-espnet/tools/installers/install_sph2pipe.sh
+tools/installers/install_sph2pipe.sh
 
 # make dummy activate
-touch espnet/tools/activate_python.sh
+touch tools/activate_python.sh
+
+# Move dataset
+mv /input/dump /home/vessl/espnet/egs/an4/tts1/dump
+mv /input/downloads /home/vessl/espnet/egs/an4/tts1/downloads
